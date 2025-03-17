ac2d4d1a4a76e45037d787ecc8c397482dc08f98
Pere Pau Carrasco Quesada <perepaucarrasco@gmail.com>
2025-03-17 11:53:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const elementoRandom = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
+
+const generadorExcusa = () => {
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+  const randomWho = elementoRandom(who);
+  const randomAction = elementoRandom(action);
+  const randomWhat = elementoRandom(what);
+  const randomWhen = elementoRandom(when);
+
+  return (`${elementoRandom(who)} ${elementoRandom(action)} ${elementoRandom(what)} ${elementoRandom(when)}`);
+};
+
+document.getElementById("excusa").innerHTML = generadorExcusa();
+
+document.getElementById("btn-generator").addEventListener("click", () => {
+  document.getElementById("excusa").innerHTML = generadorExcusa();
+});
